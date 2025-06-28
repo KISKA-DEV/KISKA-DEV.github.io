16deb61cbeea7d6ca8a859340e0b338f83fdef0b
KISKA-DEV <megamanuligor@gmail.com>
2025-06-28 22:12:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ async function loadData() {
   try {
     initTheme();
     const [racesRes, skillsRes, armorRes] = await Promise.all([
-      fetch('./Data/races.json'),
-      fetch('./Data/skills.json'),
-      fetch('./Data/armor.json')
+      fetch('Data/races.json'),
+      fetch('Data/skills.json'),
+      fetch('Data/armor.json')
     ]);
     
     races = await racesRes.json();
